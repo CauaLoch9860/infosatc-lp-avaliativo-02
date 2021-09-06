79a235aa4d30e2d2c9414fc147201ceb585bddf5
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-09-06 16:38:07 -0300

    Exer7

---
+++ 7.py
@@ -0,0 +1,24 @@
+Filmes = ["Os homens de preto","Up altas aventuras","Velozes e furiosos"]
+Jogos = ["Mine","Lol","Cs","Dota","Poe"]
+Livros = ["É agora ou nunca","O mundo é uma bola","A arte da guerra"]
+Esporte = ["Futsal","basquete","vôlei","tênis"]
+
+Filmes.append("Eu nunca")
+Filmes.insert(0,"Não case se beber")
+
+Jogos.append("Roblox")
+Jogos.insert(0,"Black desert")
+
+Livros.append("A menina q roubava livro")
+Livros.insert(0,"A cabana")
+
+Esporte.append("Corrida")
+Esporte.insert(0,"Rugbi")
+
+lista_nova = Filmes + Jogos + Livros + Esporte 
+
+
+del Esporte[0]
+
+
+print(Livros[2])
