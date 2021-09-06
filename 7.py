a2685eb95e4bc1abf5773227e29df29d1d27484b
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-09-06 16:44:22 -0300

    Exer7

--- 7.py
+++ 7.py
@@ -17,8 +17,10 @@ Esporte.insert(0,"Rugbi")
 
 lista_nova = Filmes + Jogos + Livros + Esporte 
 
+print(Livros[2])
 
 del Esporte[0]
 
+lista_nova.append("Disciplina")
+print(lista_nova)
 
-print(Livros[2])
