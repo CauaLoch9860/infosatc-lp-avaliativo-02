3c28ab5e9ce12258902a1d4097678276250427b0
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-09-06 16:38:12 -0300

    Exer8

---
+++ 8.py
@@ -0,0 +1,6 @@
+Loja = ["gabinete cooler master"," placa de vídeo GeForce RTX 2080Ti","Processador: Intel Core i5 10400",
+"Memória RAM e armazenamento: HD de 1 TB e 8 GB","Placa-Mãe: Asus EX-B460M-V5","Fonte"]
+
+del Loja[1]
+del Loja[0]
+print(Loja)
